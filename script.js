c1d9c1fc09bcf04564d722f5b8a3e4ddd37af024
bruno dykstra <brunodykstra@gmail.com>
2026-02-10 19:30:53 -0300

    incremento do viaCEP

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ document.getElementById('cep').addEventListener('blur', function(evento)=> {
     const cepInformado = elemento.value;
 
 //validar o CEP
-if (!(cepInformado.lenght ===8)){
+if (!(cepInformado.lenght ===8))
     return;
 
+//Fazer busca no viaCEP
 
-    
+fetch('https:viacep.com.br/ws/${cepinformado}/json')
+    .then(response => resnpose.json())
+    .then(data => {
+        //processamento da página
+    if(!data.error){
+        document.getElementById('logradouro').value =data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('cidade').value = data.localidade;
+        document.getElementById('estado').value = data.uf;
+
+    }else{
+        alert("CEP não encontrado")
+    }
+    })
+    .catch(error => console.error("Erro ao buscar o CEP> " , error));
+
+    )
 
 }
 //Fazer busca no VIACEP
